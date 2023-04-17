7b5a7cc8f600c9066c1b942cd262dc73623553a4
Paulo Henrique Gonçalves de Oliveira <paulo.oliveira.phgo@gmail.com>
2023-04-17 08:56:20 -0300

    atividade concluida

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
-﻿//Faça um programa que leia e valide as seguintes informações:
-
+﻿//1-Faça um programa que leia e valide as seguintes informações:
 // 1. Nome: diferente de vazio;
-
 // 2. Idade: entre 0 e 100 anos;
-
 // 3. Salário: maior que zero;
-
 // 4. Estado Civil: 's'(solteiro(a)), 'c'(casado(a)), 'v'(viuvo(a)), 'd'(divorciado(a));
 
 string nome = "";
@@ -13,6 +9,7 @@ int idade = 0;
 double salario = 0;
 char estadoCivil = ' ';
 
+Console.WriteLine($"Bem-vindo, informe seus dados: ");
 Console.Write("Informe seu nome: ");
 nome = Console.ReadLine();
 
@@ -43,86 +40,14 @@ Console.Write("Informe o seu salário: ");
 salario = double.Parse(Console.ReadLine());
 }
 
-Console.Write("Informe o seu estado civil com 's' para solteiro(a), 'c' para casado, 'v' para viuvo ou 'd' para divorciado: ");
+Console.Write("Informe o seu estado civil com 's' para solteiro(a), 'c' para casado(a), 'v' para viuvo(a) ou 'd' para divorciado(a): ");
 estadoCivil = char.Parse(Console.ReadLine());
 
 while (estadoCivil != 's' && estadoCivil != 'c' && estadoCivil != 'v' && estadoCivil != 'd') 
 {
 Console.WriteLine("Seu estado civil é invalido. Digite 's' para solteiro(a), 'c' para casado(a), 'v' para viúvo(a) ou 'd' para divorciado(a).");
-Console.Write("Informe o seu estado civil com 's' para solteiro(a), 'c' para casado, 'v' para viuvo ou 'd' para divorciado: ");
+Console.Write("Informe o seu estado civil com 's' para solteiro(a), 'c' para casado(a), 'v' para viuvo(a) ou 'd' para divorciado(a): ");
 estadoCivil = char.Parse(Console.ReadLine());
 }
 
-
-
-
-
-
-
-
-
-
-//class Program {
-   // static void Main(string[] args) {
-   //     string nome = "";
- //       int idade = 0;
- //       double salario = 0;
- //       char estadoCivil = ' ';
-//
-     //   while (nome == "") {
-    //        Console.Write("Digite o seu nome: ");
-    //        nome = Console.ReadLine();
-      //      if (nome == "") {
-     //           Console.WriteLine("Nome não pode ser vazio.");
-     //       }
-    //    }
-
-   //     while (idade < 0 || idade > 100) {
-   //         Console.Write("Digite a sua idade: ");
-     //       idade = Convert.ToInt32(Console.ReadLine());
-  //          if (idade < 0 || idade > 100) {
-      //          Console.WriteLine("Idade deve estar entre 0 e 100 anos.");
-    //        }
-    //    }
-
-      //  while (salario <= 0) {
-      //      Console.Write("Digite o seu salário: ");
- //           salario = Convert.ToDouble(Console.ReadLine());
-       //     if (salario <= 0) {
-     //           Console.WriteLine("Salário deve ser maior que zero.");
-    //        }
-    //    }
-
-      //  while (estadoCivil != 's' && estadoCivil != 'c' && estadoCivil != 'v' && estadoCivil != 'd') {
-      //      Console.Write("Digite o seu estado civil (s/c/v/d): ");
-      //      estadoCivil = Convert.ToChar(Console.ReadLine());
-      //      if (estadoCivil != 's' && estadoCivil != 'c' && estadoCivil != 'v' && estadoCivil != 'd') {
-       //         Console.WriteLine("Estado civil inválido. Digite 's' para solteiro(a), 'c' para casado(a), 'v' para viúvo(a) ou 'd' para divorciado(a).");
-    //        }
-       // }
-
-      //  Console.WriteLine("Informações válidas.");
-      //  Console.ReadKey();
-    //}
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Console.WriteLine($"Olá, {nome}, você tem {idade} anos, seu estado civil é '{estadoCivil}' e possui um salário de R${salario} reais.");
